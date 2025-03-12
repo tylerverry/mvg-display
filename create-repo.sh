f799c07922da589fe887de14d7912e8c89fa9e05
tylerverry <tyler.verry@gmail.com>
2025-03-12 17:35:46 +0100

    Remove temporary scripts and debug files

--- create-repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Create a new repository on GitHub and push your code
-gh repo create tylerverry/mvg-display --public --source=. --push
-
-echo "Repository created and code pushed successfully!"
--- test-bridge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run the Python bridge with a station ID
-STATION_ID=${1:-"de:09162:6"}  # Default to Hauptbahnhof if no arg provided
-echo "Testing bridge with station ID: $STATION_ID"
-python3 utils/mvg_bridge.py "$STATION_ID" | jq .
